5b371258e9eb36103e7ec3dff4522324dab1ffa9
dragon-bow <69964309+dragon-bow@users.noreply.github.com>
2022-01-05 01:11:47 +0300

    home-work-3

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myrecipes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.inflate
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.d("This is log for on Create")
+
+        var button_full_recipe:ImageButton = findViewById(R.id.ib_recipe1)
+        button_full_recipe.setOnClickListener {
+            val intent = Intent(this, FullRecipe::class.java)
+            startActivity(intent)
+        }
     }
 }
