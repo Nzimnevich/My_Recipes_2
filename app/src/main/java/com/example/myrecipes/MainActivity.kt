47c3f694d790375648d585a50f28231a7478e896
dragon-bow <69964309+dragon-bow@users.noreply.github.com>
2022-01-17 19:32:44 +0300

    [home-work-4]: changed image_button to image_view

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import com.example.myrecipes.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private val buttonFullRecipe:ImageButton by lazy{binding.recipe1Ibtn}
-    private val buttonFilters: ImageButton by lazy{binding.filtersIbtn}
+    private val buttonFullRecipe:ImageView by lazy{binding.recipe1Ibtn}
+    private val buttonFilters: ImageView by lazy{binding.filtersIbtn}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
