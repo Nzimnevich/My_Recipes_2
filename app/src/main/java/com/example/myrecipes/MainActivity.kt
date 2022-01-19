bb8c7792dd70eb3e604406c5d7ebec5e945f15c0
dragon-bow <69964309+dragon-bow@users.noreply.github.com>
2022-01-19 15:31:42 +0300

    [home-work-5] for fix navi

--- app/src/main/java/com/example/myrecipes/MainActivity.kt
+++ app/src/main/java/com/example/myrecipes/MainActivity.kt
@@ -6,28 +6,54 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import com.example.myrecipes.databinding.ActivityMainBinding
+import com.example.myrecipes.ui.search.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
-    private val buttonFullRecipe:ImageView by lazy{binding.recipe1Ibtn}
-    private val buttonFilters: ImageView by lazy{binding.filtersIbtn}
+    private lateinit var binding: ActivityMainBinding
+//    private val buttonFullRecipe: ImageView by lazy { binding.recipe1Ibtn }
+//    private val buttonFilters: ImageView by lazy { binding.filtersIbtn }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Timber.d("This is log for on Create")
 
 
-        buttonFullRecipe.setOnClickListener {
-            val intent = Intent(this, FullRecipeActivity::class.java)
-            startActivity(intent)
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container,SearchFragment())
+            .addToBackStack(SearchFragment::class.java.canonicalName)
+            .commit()
 
-        buttonFilters.setOnClickListener {
-            val intent = Intent(this, FilterActivity::class.java)
-            startActivity(intent)
-        }
+//        val navView: BottomNavigationView = binding.naviView
+//
+//        val listener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.search_fragment1 -> {
+//                    true
+//                }
+//                R.id.my_recipes_fragment2 -> {
+//                    true
+//                }
+//                R.id.profile_fragment3 -> {
+//                    true
+//                }
+//                else -> false
+//            }
+//
+//        }
+//        navView.setOnNavigationItemSelectedListener(listener)
+
+//        buttonFullRecipe.setOnClickListener {
+//            val intent = Intent(this, FullRecipeActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        buttonFilters.setOnClickListener {
+//            val intent = Intent(this, FilterActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
